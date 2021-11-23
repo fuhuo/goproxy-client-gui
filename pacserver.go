0c7ecca2bfb22c6c3cfa6ce81ffdc3779c70a5a2
fuhuo <blbby@qq.com>
2021-11-23 20:37:28 +0800

    【功能新增】 添加网络代理功能

--- pacserver.go
+++ pacserver.go
@@ -1,16 +1,52 @@
 package main
- 
+
 import (
 	"fmt"
 	"net/http"
 	"os"
-	"strings"
+
+	// "os/exec"
+	// "path/filepath"
 	"io/ioutil"
+	"strings"
 )
- 
+
+var host string
 var listenPort string
 var proxyPort string
- 
+
+// func PathExists(path string) (bool, error) {
+// 	_, err := os.Stat(path)
+// 	if err == nil {
+// 		return true, nil
+// 	}
+// 	if os.IsNotExist(err) {
+// 		return false, nil
+// 	}
+// 	return false, err
+// }
+
+// func GetCurrPath() string {
+// 	file, _ := exec.LookPath(os.Args[0])
+// 	path, _ := filepath.Abs(file)
+// 	splitstring := strings.Split(path, "\\")
+// 	size := len(splitstring)
+// 	splitstring = strings.Split(path, splitstring[size-1])
+// 	ret := strings.Replace(splitstring[0], "\\", "/", size-1)
+// 	return ret
+// }
+
+// func FileExists(path string) (bool, error) {
+// 	_, err := os.Stat(path)
+// 	if err == nil {
+// 		return true, nil
+// 	}
+// 	if os.IsNotExist(err) {
+// 		return false, nil
+// 	}
+// 	return false, err
+// }
+
 func ReadFile(path string) string {
 	bytes, err := ioutil.ReadFile(path)
 	if err != nil {
@@ -24,24 +60,41 @@ func ReadFile(path string) string {
 func pacHandler(w http.ResponseWriter, r *http.Request) {
 	pacfilepath := "pac.txt"
 	pactmpl := ReadFile(pacfilepath)
-	pactxt := strings.Replace(pactmpl, "__PROXY__", "PROXY 127.0.0.1:"+proxyPort+";", -1)
+	pactxt := strings.Replace(pactmpl, "__PROXY__", "PROXY "+host+":"+proxyPort+";", -1)
 	fmt.Fprintf(w, pactxt)
 }
 
-
-func main()  {
+func main() {
 	// fmt.Println(GetCurrPath())
-	if(len(os.Args[1:])>0){
-		listenPort = os.Args[1]
-		proxyPort = os.Args[2]
-	}else{
-		listenPort = "1079" 
+	if len(os.Args[1:]) > 0 {
+		host = os.Args[1]
+		listenPort = os.Args[2]
+		proxyPort = os.Args[3]
+	} else {
+		host = "127.0.0.1"
+		listenPort = "1079"
 		proxyPort = "1080"
 	}
 	fmt.Println(listenPort)
+	// path:="pac"
+	// file:="pac.txt"
+	// exist,e:=PathExists(path)
+	// if(!exist){
+	// 	e=os.Mkdir("file",0777)
+	// }
+
+	// exist,e=FileExists(path+"/"+file)
+	// if(!exist){
+	// 	f,_:=os.Create(path+"/"+file)
+	// 	_,e=f.WriteString(fmt.Sprintf("hello world!!!"))
+	// 	e=f.Close()
+	// }
+
+	// filepath := path + "/" + file
+	// http.Handle("/pac/", http.StripPrefix("/pac/", http.FileServer(http.Dir(path))))
 	http.HandleFunc("/pacfile", pacHandler)
-	e := http.ListenAndServe("127.0.0.1:" + listenPort, nil)
-	if e!=nil{
+	e := http.ListenAndServe(host+":"+listenPort, nil)
+	if e != nil {
 		fmt.Println(e.Error())
 	}
 }
